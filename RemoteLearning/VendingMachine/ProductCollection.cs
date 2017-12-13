168aa998ee3d43a4d1b3277585592426441344d1
Alin Argintaru <alin.argintaru@iquestgroup.com>
2017-12-13 18:16:24 +0200

    Added the ProductCollection class

---
+++ RemoteLearning/VendingMachine/ProductCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class ProductCollection
+    {
+        private List<Product> products;
+
+        public ProductCollection()
+        {
+            this.products = new List<Product>();
+        }
+    }
+}
